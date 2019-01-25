ecb22ee6f0974035cb8ce223c56a67a03f7391ea
Martin Milo <37755233+JavascriptFox@users.noreply.github.com>
2019-01-25 23:42:11 +0100

    Added List component (#2)
    * Added List component
    
    * Updated config.
    
    * Update styled components version.
    
    * Update travis
    
    * Final update travis
    
    * Added babel loader to install travis
    
    * Travis added babel
    
    * Travis use stable node version
    
    * Updated config
    
    * .
    
    * .
    
    * Config stuff

---
+++ src/List/index.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import styled from 'styled-components'
+import { injectStyles } from './styles'
+
+const config = require('../../config')
+
+const List = ({ children, element, items, events, ...props }) => {
+	const Element = styled[element]`
+		${injectStyles(props)}
+		::after {${props.after}}
+		::before {${props.before}}
+		:hover {${props.hover}}
+		& > * {${props.spacing}}
+		${props.style}
+	`
+	const renderChildren = (item, index) => React.isValidElement(children)
+		? React.cloneElement(children, {
+				listLength: item.length,
+				isLastItem: index === item.length - 1,
+				index,
+				item,
+			})
+		: item
+
+  return (
+    <Element className={props.className} {...events}>
+			{items.length > 0 && items.map(
+        (item, index) => renderChildren(item, index)
+      )}
+    </Element>
+  )
+}
+
+List.propTypes = {
+	items: propTypes.array,
+	element: propTypes.string,
+	events: propTypes.object,
+	display: propTypes.string,
+	wrap: propTypes.string,
+	position: propTypes.string,
+	spacing: propTypes.string,
+	padding: propTypes.string,
+	width: propTypes.string,
+	height: propTypes.string,
+	maxWidth: propTypes.string,
+	maxHeight: propTypes.string,
+	justify: propTypes.string,
+	align: propTypes.string,
+	direction: propTypes.string,
+	background: propTypes.string,
+	className: propTypes.string,
+	style: propTypes.string,
+	hover: propTypes.string,
+	before: propTypes.string,
+	after: propTypes.string,
+}
+
+List.defaultProps = {
+	items: [],
+	element: 'ul',
+	display: 'flex',
+	events: {},
+	wrap: 'nowrap',
+	margin: 'initial',
+	position: 'initial',
+	padding: 'initial',
+	width: 'auto',
+	height: 'auto',
+	maxWidth: 'initial',
+	maxHeight: 'initial',
+	justify: 'initial',
+	align: 'initial',
+	direction: 'column',
+	background: 'transparent',
+	className: '',
+	spacing: '',
+	style: '',
+	hover: '',
+	before: '',
+	after: '',
+	[config.breakpoints[0].size]: 100,
+}
+
+export default List
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import Box from './Box'
+import Row from './Row'
+import List from './List'
 
 export {
-	Box
+	Box,
+	Row,
+	List,
 }
