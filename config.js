c04a18ef82be32396c06e8e5fede5d34f30a54e3
martin.milo <martin.milo@finnology.com>
2019-01-26 21:27:24 +0100

    Update config

--- config.js
+++ config.js
@@ -4,16 +4,19 @@ const dir = __dirname
 const getDirectory = path => dir && dir.includes(path)
 	? dir.slice(0, dir.indexOf(path))
 	: dir
+const moduloxConfigFile = `${getDirectory('/node_modules')}/modulox.config.json`
 
-module.exports = fse.existsSync(`${getDirectory('/node_modules')}/modulox.config.json`)
-	? fse.readFileSync(`${getDirectory('/node_modules')}/modulox.config.json`, 'utf8')
-	: {
-		breakpoints: [
-			{ size: 'xs', prefix: 's:', minWidth: 0 },
-			{ size: 'xm', prefix: 'm:', minWidth: 565 },
-			{ size: 'xt', prefix: 't:', minWidth: 769 },
-			{ size: 'xl', prefix: 'l:', minWidth: 1200 },
-			{ size: 'xd', prefix: 'd:', minWidth: 1980 }
-		],
-		splitter: ','
-	}
+module.exports = function() {
+	return fse.existsSync(moduloxConfigFile)
+		? fse.readFileSync(moduloxConfigFile, 'utf8')
+		: {
+			breakpoints: [
+				{ size: 'xs', prefix: 's:', minWidth: 0 },
+				{ size: 'xm', prefix: 'm:', minWidth: 565 },
+				{ size: 'xt', prefix: 't:', minWidth: 769 },
+				{ size: 'xl', prefix: 'l:', minWidth: 1200 },
+				{ size: 'xd', prefix: 'd:', minWidth: 1980 }
+			],
+			splitter: ','
+		}
+}
