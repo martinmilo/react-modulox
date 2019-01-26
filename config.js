22aa473e7939562b5f6e9733ba8356624feaa788
martin.milo <martin.milo@finnology.com>
2019-01-26 14:48:09 +0100

    Update config readFileSync

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 const fs = require('fs-extra')
 
-const config = fs.existsSync('./modulox.config.js')
+fs.existsSync('./modulox.config.js')
 	? fs.readFileSync('./modulox.config.js', 'utf8')
 	: fs.readFileSync('./default.config.js', 'utf8')
-
-module.exports = config
