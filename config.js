3f48ae4140fb19a1fc585c41912c4effb3239279
martin.milo <martin.milo@finnology.com>
2019-01-26 13:25:46 +0100

    - Added config for CLI - Updated README.md

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
-if (require('./modulox.config')) {
-	module.exports = require('./modulox.config')
-}
-module.exports = require('./default.config')
+const fs = require('fs-extra')
+
+const config = fs.existsSync('./modulox.config')
+	? fs.readFileSync('./modulox.config', 'utf8')
+	: fs.readFileSync('./default.config', 'utf8')
+
+module.exports = config
