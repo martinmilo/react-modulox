9b80f7c763bd44b1cbb72cef4625d0db99aad47f
martin.milo <martin.milo@finnology.com>
2019-03-03 11:54:44 +0100

    - Fixed list keys - Changed config for smallest screen size

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -5,68 +5,78 @@ import { injectStyles } from './styles'
 import { commonTypes } from '../../types'
 import theme from '../../theme/'
 
-const List = ({ children, element, items, events, ...props }) => {
-	const Element = styled[element]`
-		${injectStyles(props)}
-		::after {${props.after}}
-		::before {${props.before}}
-		:hover {${props.hover}}
-		& > * {
-			margin: ${props.spacing};
-			${props.childs}
-		}
-		${props.style}
-	`
-	const renderChildren = (item, index) => React.isValidElement(children)
-		? React.cloneElement(children, {
-				listLength: items.length,
-				isLastItem: index === items.length - 1,
-				index,
-				item,
-			})
-		: item
+const List = ({ children, element, items, useKey, events, ...props }) => {
+  const Element = styled[element]`
+    ${injectStyles(props)}
+    ::after {
+      ${props.after}
+    }
+    ::before {
+      ${props.before}
+    }
+    :hover {
+      ${props.hover}
+    }
+    & > * {
+      margin: ${props.spacing};
+      ${props.childs}
+    }
+    ${props.style}
+  `
+  const renderChildren = (item, index) =>
+    React.isValidElement(children) ? (
+      React.cloneElement(children, {
+        key: item[useKey] || index,
+        listLength: items.length,
+        isLastItem: index === items.length - 1,
+        index,
+        item
+      })
+    ) : (
+      <li key={index}>{item}</li>
+    )
 
   return (
     <Element className={props.className} {...events}>
-			{items.length > 0 && items.map(
-        (item, index) => renderChildren(item, index)
-      )}
+      {items.length > 0 && items.map((item, index) => renderChildren(item, index))}
     </Element>
   )
 }
 
 List.propTypes = {
-	items: propTypes.array,
-	...commonTypes,
+  items: propTypes.array,
+  useKey: propTypes.string,
+  ...commonTypes
 }
 
 List.defaultProps = {
-	items: [],
-	element: 'ul',
-	display: 'flex',
-	events: {},
-	wrap: 'nowrap',
-	margin: 'initial',
-	position: 'initial',
-	padding: 'initial',
-	width: 'auto',
-	height: 'auto',
-	maxWidth: 'initial',
-	maxHeight: 'initial',
-	justify: 'initial',
-	align: 'initial',
-	direction: 'column',
-	background: 'transparent',
-	transition: ``,
-	className: ``,
-	spacing: ``,
-	style: ``,
-	hover: ``,
-	before: ``,
-	after: ``,
-	childs: ``,
-	xx: 100,
-	theme,
+  items: [],
+  useKey: 'id',
+  element: 'ul',
+  display: 'flex',
+  events: {},
+  wrap: 'nowrap',
+  margin: 'initial',
+  position: 'initial',
+  padding: 'initial',
+  width: 'auto',
+  height: 'auto',
+  maxWidth: 'initial',
+  maxHeight: 'initial',
+  justify: 'initial',
+  align: 'initial',
+  direction: 'column',
+  background: 'transparent',
+  transition: ``,
+  className: ``,
+  spacing: ``,
+  style: ``,
+  hover: ``,
+  before: ``,
+  after: ``,
+  childs: ``,
+  xt: 100,
+  theme
 }
 
 export default List
